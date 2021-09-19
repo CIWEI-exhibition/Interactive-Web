2376cc0200fb6f720f94cb511fffce2c274bb35c
hyj0705 <hyj0705a@gmail.com>
2021-09-19 23:05:27 +0900

    no message

--- Exhibition_Page/page2/js/page2.js
+++ Exhibition_Page/page2/js/page2.js
@@ -4,7 +4,7 @@ let mx = 0;
 let my = 0;
 let speed = 0.03;
 let scrollTop = 0;
-let bg, bgWall, bgInterior, com, comChair, comLight, fgMoonlight, fg;
+let bg, bgWall, bgInterior, com, comMonitor, comChair, comLight, fgMoonlight, fg;
 
 window.onload = function(){
     progressBar = document.getElementsByClassName("bar")[0];
@@ -42,7 +42,7 @@ function scrollFunc(e){
     comLight.style.transform = "translate3d(0px ," + scrollTop * .47 +"px , 0px)";
     comChair.style.transform = "translate3d(0px ," + scrollTop * .45 +"px , 0px)";
     fgMoonlight.style.transform = "translate3d(0px ," + scrollTop * .55 +"px , 0px);"
-    fg.style.transform = "translate3d(0px ," + scrollTop * 1.5 +"px , 0px)";
+    fg.style.transform = "translate3d(0px ," + scrollTop * .12 +"px , 0px)";
 }
 
 function stageResize() {        //per를 위한 함수 (document랑 window height값)
@@ -50,16 +50,16 @@ function stageResize() {        //per를 위한 함수 (document랑 window heigh
     _windowNum = window.outerHeight;
 }
 
-function loop() {
-    mx += (x - mx) * speed;
-    my += (y - my) * speed;
+// function loop() {
+//     mx += (x - mx) * speed;
+//     my += (y - my) * speed;
 
-    bgWall.style.transform = "translate("+ mx / 30 +"px ," + -my / 30 +"px)";
-    fgMoonlight.style.transform = "translate("+ mx / 50 +"px ," + -scrollTop * .85 +"px)";
-    fg.style.transform = "translate("+ -mx / 20 +"px ," + -my / 20 +"px)";
+//     bgWall.style.transform = "translate("+ mx / 30 +"px ," + -my / 30 +"px)";
+//     fgMoonlight.style.transform = "translate("+ mx / 50 +"px ," + -scrollTop * .85 +"px)";
+//     fg.style.transform = "translate("+ -mx / 20 +"px ," + -my / 20 +"px)";
 
-    window.requestAnimationFrame(loop);
-}
+//     window.requestAnimationFrame(loop);
+// }
 
 
 function mouseMove (e) {        //윈도우 width의 가운데 기준
