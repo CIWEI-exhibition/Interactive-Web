2d67168127786be1cbedbd06bdd1d3d47ed8748c
Subin <80036437+hansb1023@users.noreply.github.com>
2021-09-21 13:56:32 +0900

    우선 커밋.... 팝업창 왜 안돼애ㅐ애ㅐㅐ

--- Exhibition_Page/page2/+pop/index_plus_popup.js
+++ Exhibition_Page/page2/+pop/index_plus_popup.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-    $('.trigger').on('click', function() {
-        $('.modal-wrapper').toggleClass('open');
-        $('.wrapper').toggleClass('blur-it');
-        return false;
+// window.onload = function($){
+//     document.getElementById('monitor').onclick = function(){
+//         $(".modal-wrapper").toggleClass("open");
+//         $(".wrapper").toggleClass("blur-it");
+//         return false;
+//     };
+// };
+
+$(function(){
+    $("#modal-open").click(function(){        
+        $("#popup").css('display','flex').hide().fadeIn();
+        $(".wrapper").toggleClass("blur-it");
     });
-});
+    $("#close").click(function(){
+        modalClose();
+    });
+    function modalClose(){
+      $("#popup").fadeOut();
+    }
+  });
