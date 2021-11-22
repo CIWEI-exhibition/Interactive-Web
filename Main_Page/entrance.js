6303f0652992b230530a57ec7c030d0c9eba4bf4
hyj0705 <hyj0705a@gmail.com>
2021-11-22 10:48:31 +0900

    [update] 예나 개인전시 배치 완료

---
+++ Main_Page/entrance.js
@@ -0,0 +1,26 @@
+// 팝업창
+function open_pop1(flag){
+    $('#Modal1').css('display','block');
+};
+function open_pop2(flag){
+    $('#Modal2').css('display','block');
+};
+function open_pop3(flag){
+    $('#Modal3').css('display','block');
+};
+function open_pop4(flag){
+    $('#Modal4').css('display','block');
+};
+  
+function close_pop1(flag) {
+    $('#Modal1').hide();
+};
+function close_pop2(flag) {
+    $('#Modal2').hide();
+};
+function close_pop3(flag) {
+    $('#Modal3').hide();
+};
+function close_pop4(flag) {
+    $('#Modal4').hide();
+};
