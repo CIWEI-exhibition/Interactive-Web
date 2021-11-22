c8afcd01cf393d357b2185bed894b8332e01227d
Subin <80036437+hansb1023@users.noreply.github.com>
2021-11-22 22:27:25 +0900

    수정

--- Exhibition_Page/page1.5/page2.js
+++ Exhibition_Page/page1.5/page2.js
@@ -128,3 +128,38 @@ function scrollRotate(){
     image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
 }
 
+//-------------------------------------------------------------------------
+//채팅창 스크롤
+// Scroll Animation (sa, 스크롤 애니메이션)
+// const saDefaultMargin = 300;
+// let saTriggerMargin = 0;
+// let saTriggerHeight = 0;
+// const saElementList = document.querySelectorAll('.chat');
+
+// const saFunc = function() {
+//     for (const element of saElementList) {
+//         if (!element.classList.contains('show')) {
+//             if (element.dataset.saMargin) {
+//                 saTriggerMargin = parseInt(element.dataset.saMargin);
+//             } else {
+//                 saTriggerMargin = saDefaultMargin;
+//             }
+
+//             if (element.dataset.saTrigger) {
+//                 saTriggerHeight = document.querySelector(element.dataset.saTrigger).getBoundingClientRect().top + saTriggerMargin;
+//             } else {
+//                 saTriggerHeight = element.getBoundingClientRect().top + saTriggerMargin;
+//             }
+
+//             if (window.innerHeight > saTriggerHeight) {
+//                 let delay = (element.dataset.saDelay) ? element.dataset.saDelay : 0;
+//                 setTimeout(function() {
+//                     element.classList.add('show');
+//                 }, delay);
+//             }
+//         }
+//     }
+// }
+
+// window.addEventListener('load', saFunc);
+// window.addEventListener('scroll', saFunc);
