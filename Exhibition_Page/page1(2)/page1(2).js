17c8899862c37f0593d7a9d496c2cfd41d35e8e4
hyj0705 <hyj0705a@gmail.com>
2021-11-24 03:45:18 +0900

    [update] 음악 수정 + 2페이지 음악 제작

--- Exhibition_Page/page1(2)/page1(2).js
+++ Exhibition_Page/page1(2)/page1(2).js
@@ -1,18 +1,18 @@
 
-var sound = new Howl({
-    src: ['total.mp3'],
-    volume: 1,
-    autoplay: true,
-    loop: true,
-    onend : () => {     //오디오가 재생된 다음 실행할 내용
-        console.log('Finished!');
-    }
-});
-
-sound.play();
-
 window.onload = function(){
 
+    var sound = new Howl({
+        src: ['total.mp3'],
+        volume: 1,
+        autoplay: true,
+        loop: true,
+        onend : () => {     //오디오가 재생된 다음 실행할 내용
+            console.log('Finished!');
+        }
+    });
+    
+    sound.play();
+    
     // 부스 쪽
     var bgSky = document.querySelector(".bgSky");
     var bgStar1 = document.querySelector(".bgStar1");
