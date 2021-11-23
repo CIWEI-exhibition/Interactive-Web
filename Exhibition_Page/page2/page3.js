583662b3dbba046b517dd0d938962191bcf5168e
hyj0705 <hyj0705a@gmail.com>
2021-11-24 04:10:01 +0900

    [update] 2페이지(방) 음원 삽입 완료!

--- Exhibition_Page/page2/page3.js
+++ Exhibition_Page/page2/page3.js
@@ -5,6 +5,19 @@ let my = 0;
 let speed = 0.03;
 
 window.onload = function(){
+    
+    var sound = new Howl({
+        src: ['stand.mp3'],
+        volume: 1,
+        autoplay: true,
+        loop: true,
+        onend : () => {     //오디오가 재생된 다음 실행할 내용
+            console.log('Finished!');
+        }
+    });
+
+    sound.play();
+
     // progressBar = document.getElementsByClassName("bar")[0];
 
     //이미지들 연결
