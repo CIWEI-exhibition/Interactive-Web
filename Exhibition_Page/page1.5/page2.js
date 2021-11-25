24f0ed67a00a0a72fa8aad0f34fb185b430b28ab
hyj0705 <hyj0705a@gmail.com>
2021-11-25 18:36:41 +0900

    [update] 게임 오류 원인 발견/사운드는 계속 제작중

--- Exhibition_Page/page1.5/page2.js
+++ Exhibition_Page/page1.5/page2.js
@@ -72,60 +72,27 @@ window.onload = function(){
 
 }
 
-var sound2 = new Howl({
-    src: ['messenger_1.mp3'],
-    volume: 1,
-    autoplay: true,
-    onend : () => {     //오디오가 재생된 다음 실행할 내용
-        console.log('Finished!');
-    }
-});
-
-if(chat1.style.opacity == 1){
+function messengerSound(){
+    var sound2 = new Howl({
+        src: ['messenger_1.mp3'],
+        volume: 1,
+        autoplay: true,
+        onend : () => {     //오디오가 재생된 다음 실행할 내용
+            console.log('Finished!');
+        }
+    });
     sound2.play();
 }
-function scrollFunc(e){
-    var scrollTop = document.documentElement.scrollTop;
 
-//     let per = Math.ceil(scrollTop / (_documentHum - _windowNum) * 100);
-//     progressBar.style.height = per + "%";   //세로 진행 바 위해
-
-//     //패럴렉스 핵심
-//     bg.style.transform = "translate3d(0px ," + scrollTop * .12 +"px, 0px)";
-//     bgWall.style.transform = "translate3d(0px ," + scrollTop * .35 +"px , 0px)";
-//     bgInterior.style.transform = "translate3d(0px ," + scrollTop * .35 +"px , 0px)";
-//     com.style.transform = "translate3d(0px ," + scrollTop * .45 +"px , 0px)";
-
-//     // stand1.style.transform = "translate3d(0px ," + scrollTop * .45 +"px , 0px)";
-//     // stand2.style.transform = "translate3d(0px ," + scrollTop * .45 +"px , 0px)";
-
-//     comLight.style.transform = "translate3d(0px ," + scrollTop * .47 +"px , 0px)";
-//     comChair.style.transform = "translate3d(0px ," + scrollTop * .43 +"px , 0px)";
-//     fgMoonlight.style.transform = "translate3d(0px ," + scrollTop * .55 +"px , 0px);"
-//     fg.style.transform = "translate3d(0px ," + scrollTop * 1.55 +"px , 0px)";
+if(chat1.style.opacity == 1){
+    messengerSound();
 }
+
 
 function stageResize() {        //per를 위한 함수 (document랑 window height값)
     _documentHum = document.body.offsetHeight;
     _windowNum = window.outerHeight;
 }
-
-// function loop() {
-//     mx += (x - mx) * speed;
-//     my += (y - my) * speed;
-
-//     sub.style.transform = "translate("+ mx / 30 +"px ," + -my / 30 +"px)";
-//     subHandle.style.transform = "translate("+ mx / 50 +"px ," + -scrollTop * .85 +"px)";
-//     subSeat.style.transform = "translate("+ -mx / 20 +"px ," + -my / 20 +"px)";
-
-//     window.requestAnimationFrame(loop);
-// }
-
-
-// function mouseMove (e) {        //윈도우 width의 가운데 기준
-//     x = (e.clientX - window.innerHeight / 2);
-//     y = (e.clientY - window.innerHeight / 2);
-// }
 
 //------------------------------------------------------------------------------------------------
 //전시 화면 fade out되는 코드
@@ -150,12 +117,6 @@ $(document).ready(function(){
     
     });
 })
-
-//손잡이 애니메이션(버전2)
-// function scrollRotate(){
-//     let image = getElementById("subway-handle1");
-//     image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
-// }
 
 //-------------------------------------------------------------------------
 //채팅창 애니메이션
