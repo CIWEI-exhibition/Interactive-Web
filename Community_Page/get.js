b9e040c1cce1399191d72e345f734d0d2a6a7fdb
Subin <80036437+hansb1023@users.noreply.github.com>
2021-12-01 13:20:17 +0900

    axios 변경

--- Community_Page/get.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import abd from './a.json';
-
-axios.get('https://www.aquaurore.co.kr/Community_Page/note.html').then((Response)=>{
-    console.log(Response.data);
-}).catch((Error)=>{
-    console.log(Error);
-})
