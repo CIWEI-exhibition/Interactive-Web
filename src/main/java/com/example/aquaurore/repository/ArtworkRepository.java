dc5f8cf5157052bcfe3f52c152a5b908d0e437c2
Subin <80036437+hansb1023@users.noreply.github.com>
2026-08-24 19:57:38 +0900

    [fix] ArtworkRepository @EntityGraph 누락 커밋

--- src/main/java/com/example/aquaurore/repository/ArtworkRepository.java
+++ src/main/java/com/example/aquaurore/repository/ArtworkRepository.java
@@ -1,11 +1,16 @@
 package com.example.aquaurore.repository;
 
 import com.example.aquaurore.domain.Artwork;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ArtworkRepository extends JpaRepository<Artwork, Long> {
+
+    @EntityGraph(attributePaths = "artist")
     List<Artwork> findAllByOrderByDisplayOrderAsc();
+
+    @EntityGraph(attributePaths = "artist")
     List<Artwork> findByArtistId(Long artistId);
 }
